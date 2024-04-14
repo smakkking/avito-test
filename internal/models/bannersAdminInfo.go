e43ad1e8a542223ff1260cabb06e76f234d59627
andreysm <smirnov.andrey79@wildberries.work>
2024-04-14 13:03:31 +0300

    новый тип для json-баннера

--- internal/models/bannersAdminInfo.go
+++ internal/models/bannersAdminInfo.go
@@ -1,6 +1,11 @@
 package models
 
-import "time"
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"time"
+)
 
 type BasicBannnerInfo struct {
 	TagIDs    []int       `json:"tag_ids"`
@@ -14,4 +19,23 @@ type BannerInfo struct {
 	BannerID  int       `json:"banner_id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type BannerContent map[string]interface{}
+
+// Make the Attrs struct implement the driver.Valuer interface. This method
+// simply returns the JSON-encoded representation of the struct.
+func (a BannerContent) Value() (driver.Value, error) {
+	return json.Marshal(a)
+}
+
+// Make the Attrs struct implement the sql.Scanner interface. This method
+// simply decodes a JSON-encoded value into the struct fields.
+func (a *BannerContent) Scan(value interface{}) error {
+	b, ok := value.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+
+	return json.Unmarshal(b, &a)
 }
